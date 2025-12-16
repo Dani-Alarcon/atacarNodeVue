ddc3a714efd8d0274f4144318ad0051c785bd340
Dani-Alarcon <daniel.alarcon.7e6@itb.cat>
2025-12-16 18:21:26 +0100

    codigo medio adaptado

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+    pokemon: {
+        type: Object,
+        require: true
+    }
+
+})
+</script>
+
+<template>
+    <p>Pokemons</p>
+    <div>    
+        <img :src="props.pokemon.imatge" :alt="props.pokemon.name">
+        <h3>{{ props.pokemon.name }}</h3>
+        <p>{{ props.pokemon.type }} - {{ props.pokemon.generation }}</p>
+    </div>
+    <!--<RouterLink :to="{ name: 'mealDetail', params: { idMeal: props.pokemon.id } }">Recepta</RouterLink>-->
+    
+    <hr>
+
+
+</template>
+
+<style scoped>
+img {
+    height: 150px;
+}
+</style>
